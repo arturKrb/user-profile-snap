207ea222fa22c5ea25c0e2efa6e32153a2399af7
wof-labs <navin@navin.ee>
2022-09-07 13:58:09 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Description
+
+This is an example Snap to store your personal data. Forked from [@metamask/template-snap(https://github.com/MetaMask/template-snap).
+For more detailed instructions, see [the MetaMask documentation](https://docs.metamask.io/guide/snaps.html#serving-a-snap-to-your-local-environment).
+Example usecase: E-commerce quick checkout feature
+
+### Setup
+
+```shell
+yarn install
+```
+
+### Testing and Linting
+
+This example Snap has no included tests
+
+Run `yarn lint` to run the linter, or run `yarn lint:fix` to run the linter and fix any automatically fixable issues.
---
+++ src/index.js
@@ -0,0 +1,43 @@
+async function getProfile() {
+  const state = await wallet.request({
+    method: 'snap_manageState',
+    params: ['get'],
+  });
+  return state.profile;
+}
+
+module.exports.onRpcRequest = async ({ request }) => {
+  switch (request.method) {
+    case 'confirm': {
+      const result = await wallet.request({
+        method: 'snap_confirm',
+        params: [request.params],
+      });
+      return result;
+    }
+
+    case 'updateProfile': {
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['update', { profile: request.params }],
+      });
+      return await getProfile();
+    }
+
+    case 'getUserProfile': {
+      return await getProfile();
+    }
+
+    case 'clearUserProfile': {
+      // Use update instead of clear to update only profile state
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['update', { profile: {} }],
+      });
+      return await getProfile();
+    }
+
+    default:
+      throw new Error('Method not found.');
+  }
+};
